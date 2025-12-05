3a236fb1c1e9ab722d7e513125c8bcd7c05c4289
xXmarcosX <marcos.v3567@gmail.com>
2025-12-05 13:12:33 -0300

    DepartmentDao and findDepartmentById implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.enitites.Department;
 import model.enitites.Seller;
 
@@ -10,26 +12,28 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        DepartmentDao depDao = DaoFactory.createDepartmentDao();
         Department dep = new Department(1, "Computers");
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = sellerDao.findById(11);
 
         List<Seller> sellers = sellerDao.findByDepartment(dep);
         List<Seller> sellers2 = sellerDao.findAll();
 
-        //TEST FIND BY ID
+        //TEST FIND BY SELLER ID
         System.out.println("Test ID");
         System.out.println(seller);
 
-        //TEST FIND BY DEPARTMENT
+        //TEST FIND SELLER BY DEPARTMENT
         System.out.println("\n\nTest find by department");
         sellers.forEach(System.out::println);
 
-        //TEST FIND ALL
+        //TEST FIND ALL SELLERS
         System.out.println("\n\nTest find all");
         sellers2.forEach(System.out::println);
 
-        //TEST INSERT
+        //TEST INSERT SELLER
         System.out.println("\n\nTest insert");
         Seller newSeller = new Seller(null, "Chris", "chris@gmail.com", new Date(), 4000.0, dep);
 
@@ -37,7 +41,7 @@ public class Main {
 
         System.out.println("New seller ID: " + newSeller.getId());
 
-        //TEST UPDATE
+        //TEST UPDATE SELLER
         System.out.println("\n\nTest update");
         seller.setBaseSalary(6000.0);
 
@@ -45,11 +49,16 @@ public class Main {
 
         System.out.println(seller.getBaseSalary());
 
-        //TEST DELETE
+        //TEST DELETE SELLER
         System.out.println("\n\nTest Delete");
 
         sellerDao.deleteById(newSeller.getId());
 
         System.out.println(sellerDao.findById(newSeller.getId()));
+
+        System.out.println("\n");
+
+        //TEST GET DEPARTMENT BY ID
+        System.out.println(depDao.findById(1));
     }
 }
---
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,69 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.enitites.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {this.conn = conn;}
+
+    @Override
+    public void insert(Department department) {
+
+    }
+
+    @Override
+    public void update(Department department) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department dp = new Department();
+
+                dp.setName(rs.getString("name"));
+                dp.setId(rs.getInt("Id"));
+
+                return dp;
+            }
+
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
